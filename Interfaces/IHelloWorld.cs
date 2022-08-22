5fbd9654e98ab81e1f9795742df592a3544fb5fe
stas <79771234+ninahur1989@users.noreply.github.com>
2022-08-22 22:26:29 +0300

    ready to PR

---
+++ Interfaces/IHelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace User.api.Interfaces
+{
+    public interface IHelloWorld
+    {
+        public Task<ViewResult> CreateAsync();
+        public Task<ViewResult> ListAsync();
+        public Task<ActionResult> UpdateAsync(int id);
+        public Task<ActionResult> DeleteAsync(int id);
+    }
+}
